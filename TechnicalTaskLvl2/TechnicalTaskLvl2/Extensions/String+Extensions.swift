3f0f653b04ad7f34b06b01aac584b4c4009147a7
k-hanizoda <karalina.hanizoda@innowise.com>
2024-12-30 13:03:12 +0100

    FIX: Update validation for email

--- TechnicalTaskLvl2/TechnicalTaskLvl2/Extensions/String+Extensions.swift
+++ TechnicalTaskLvl2/TechnicalTaskLvl2/Extensions/String+Extensions.swift
@@ -2,20 +2,9 @@ import Foundation
 
 extension String {
     func isValidEmail() -> Bool {
-        let emailPattern = "^(?=[a-zA-Z0-9@._%+-]{1,254}$)(?!.*@.*@)(?!.*\\.{2})[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]{2,})+$"
+        let emailPattern = "^(?=[^@]{1,254}$)(?!.*@.*@)(?!.*\\.{2})[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,63}$|^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[\\p{L}]{2,63}$"
         
         let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailPattern)
-        if !emailPredicate.evaluate(with: self) {
-            return false
-        }
-        
-        let components = self.split(separator: "@")
-        if let domain = components.last, let firstCharacter = domain.first {
-            if firstCharacter.isNumber {
-                return false
-            }
-        }
-        
-        return true
+        return emailPredicate.evaluate(with: self)
     }
 }
